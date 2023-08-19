622d4d6ad90fc123cf79c0a4fa73079ef7f4f096
Armando Terán Castillo <desarrollo1GV@outlook.com>
2023-08-19 11:08:17 -0600

    Add Tailwind to starting project

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,4 +1,6 @@
+<h1 class="caret-amber-200 font-bold">This is my Navbar</h1>
 <nav>
-    <a href="/home">homeScreen</a>
+    <a class="text-blue-500 hover:text-fuchsia-500" href="/home">homeScreen</a>
     <a href="/player">playerScreen</a>
 </nav>
+
